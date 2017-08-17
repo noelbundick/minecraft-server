bf799636bf77bc45ce573cd98225a66b6f9a93a2
Noel Bundick <nobun@microsoft.com>
2017-08-17 11:20:50 -0700

    More fixes for alpine-custom

--- alpine-custom/customstart.sh
+++ alpine-custom/customstart.sh
@@ -4,6 +4,7 @@ set -e
 if [ -z "$(ls -A /data)" ]; then
    echo "Empty data volume detected. Populating with default world"
    cp -r /default/* /data
+   chown -R minecraft:minecraft /data
 fi
 
 . /start
