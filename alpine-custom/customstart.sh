ea82ce5fcdf15b3798290cf512f8b74764ad4879
Noel Bundick <nobun@microsoft.com>
2017-08-21 09:54:19 -0700

    Update alpine-custom with container-upgrade state

--- alpine-custom/customstart.sh
+++ alpine-custom/customstart.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 set -e
 
 if [ -z "$(ls -A /data)" ]; then
@@ -7,4 +7,8 @@ if [ -z "$(ls -A /data)" ]; then
    chown -R minecraft:minecraft /data
 fi
 
-. /start
+nohup /start >/minecraft.out & 2>&1 \
+  && sleep 2s && tail -n +0 -f /minecraft.out & 2>&1 \
+  && grep -q "RCON running" <(tail -f /minecraft.out) \
+  && rcon-cli --host 127.0.0.1 --port 25575 --password cheesesteakjimmys ban b973ece7-93e7-477e-a69a-d22554953e89 \
+  && wait $(pidof start)
