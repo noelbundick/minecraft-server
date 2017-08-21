f00cc46b6025c00060486daae222471e3fbeccca
Noel Bundick <nobun@microsoft.com>
2017-08-21 12:34:19 -0700

    Changes for nanoserver-custom-0.0.2

--- alpine-custom/customstart.sh
+++ alpine-custom/customstart.sh
@@ -7,8 +7,9 @@ if [ -z "$(ls -A /data)" ]; then
    chown -R minecraft:minecraft /data
 fi
 
-nohup /start >/minecraft.out & 2>&1 \
-  && sleep 2s && tail -n +0 -f /minecraft.out & 2>&1 \
-  && grep -q "RCON running" <(tail -f /minecraft.out) \
-  && rcon-cli --host 127.0.0.1 --port 25575 --password cheesesteakjimmys ban b973ece7-93e7-477e-a69a-d22554953e89 \
-  && wait $(pidof start)
+nohup /start >/minecraft.out & 2>&1
+sleep 2s
+tail -n +0 -f /minecraft.out & 2>&1
+grep -q "RCON running" <(tail -f /minecraft.out)
+rcon-cli --host 127.0.0.1 --port 25575 --password cheesesteakjimmys ban b973ece7-93e7-477e-a69a-d22554953e89
+wait $(pidof start)
