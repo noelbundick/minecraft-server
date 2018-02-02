fbe97e7b01098100cc1ff3dc081d9c76f63d25fd
Noel Bundick <noelbundick@gmail.com>
2018-02-02 09:00:27 -0800

    Add openhack 1709 images

---
+++ openhack/2.0/nanoserver-1709/customstart.ps1
@@ -0,0 +1,15 @@
+$ErrorActionPreference = 'Stop'
+$ProgressPreference = 'SilentlyContinue'
+
+if((Get-ChildItem G:\ -Force | Select-Object -First 1 | Measure-Object).Count -eq 0)
+{
+   echo "Empty data volume detected. Populating with default world"
+   cp -r C:\default\* G:
+}
+
+Start-Job -ScriptBlock {
+  while((Select-String -Pattern 'RCON running' -Path C:\minecraft\minecraft.out) -eq $null) { Write-Output "nothing"; Start-Sleep -Seconds 1 }
+  rcon-cli --host 127.0.0.1 --port 25575 --password cheesesteakjimmys ban b973ece7-93e7-477e-a69a-d22554953e89
+} | Out-Null
+
+powershell -File C:\minecraft\start.ps1 | Tee-Object C:\minecraft\minecraft.out
