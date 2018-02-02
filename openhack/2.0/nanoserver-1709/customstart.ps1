ac15e61f148c9e7566238229b6786a8d56a5245b
Noel Bundick <noelbundick@gmail.com>
2018-02-02 10:22:42 -0800

    Fix 2.0-nanoserver-1709 for real

--- openhack/2.0/nanoserver-1709/customstart.ps1
+++ openhack/2.0/nanoserver-1709/customstart.ps1
@@ -8,7 +8,7 @@ if((Get-ChildItem G:\ -Force | Select-Object -First 1 | Measure-Object).Count -e
 }
 
 Start-Job -ScriptBlock {
-  while((Select-String -Pattern 'RCON running' -Path C:\minecraft\minecraft.out) -eq $null) { Write-Output "nothing"; Start-Sleep -Seconds 1 }
+  while(!(Test-Path C:\minecraft\minecraft.out) -or (Select-String -Pattern 'RCON running' -Path C:\minecraft\minecraft.out) -eq $null) { Write-Output "nothing"; Start-Sleep -Seconds 1 }
   rcon-cli --host 127.0.0.1 --port 25575 --password cheesesteakjimmys ban b973ece7-93e7-477e-a69a-d22554953e89
 } | Out-Null
 
